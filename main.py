54d7a2ad6c34271e547b0ca184b18e4b84edc7e6
sHiLo BaBilA <shilobabila1@gmail.com>
2023-06-01 23:14:51 +0300

    test1

---
+++ main.py
@@ -0,0 +1,102 @@
+import numpy as np 
+from tensorflow import keras
+from PIL import Image
+from tensorflow.keras.preprocessing.image import ImageDataGenerator, img_to_array, array_to_img, load_img
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.applications import VGG19
+from tensorflow.keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+from gtts import gTTS
+
+batch_size = 1
+seed = 42
+
+classes = { 0:'Speed limit (20km/h)',
+            1:'Speed limit (30km/h)', 
+            2:'Speed limit (50km/h)', 
+            3:'Speed limit (60km/h)', 
+            4:'Speed limit (70km/h)', 
+            5:'Speed limit (80km/h)', 
+            6:'End of speed limit (80km/h)', 
+            7:'Speed limit (100km/h)', 
+            8:'Speed limit (120km/h)', 
+            9:'No passing', 
+            10:'No passing veh over 3.5 tons', 
+            11:'Right-of-way at intersection', 
+            12:'Priority road', 
+            13:'Yield', 
+            14:'Stop', 
+            15:'No vehicles', 
+            16:'Veh > 3.5 tons prohibited', 
+            17:'No entry', 
+            18:'General caution', 
+            19:'Dangerous curve left', 
+            20:'Dangerous curve right', 
+            21:'Double curve', 
+            22:'Bumpy road', 
+            23:'Slippery road', 
+            24:'Road narrows on the right', 
+            25:'Road work', 
+            26:'Traffic signals', 
+            27:'Pedestrians', 
+            28:'Children crossing', 
+            29:'Bicycles crossing', 
+            30:'Beware of ice/snow',
+            31:'Wild animals crossing', 
+            32:'End speed + passing limits', 
+            33:'Turn right ahead', 
+            34:'Turn left ahead', 
+            35:'Ahead only', 
+            36:'Go straight or right', 
+            37:'Go straight or left', 
+            38:'Keep right', 
+            39:'Keep left', 
+            40:'Roundabout mandatory', 
+            41:'End of no passing', 
+            42:'End no passing veh > 3.5 tons' }
+height = 50
+width = 50
+batch_size = 150
+
+model = keras.models.Sequential([    
+    keras.layers.Conv2D(filters=16, kernel_size=(5,5), activation='relu', input_shape=(50,50,3)),
+    keras.layers.Conv2D(filters=32, kernel_size=(5,5), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+    
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation='relu'),
+    keras.layers.MaxPool2D(pool_size=(2, 2)),
+    keras.layers.BatchNormalization(axis=-1),
+    keras.layers.Dropout(rate=0.25),
+    keras.layers.Flatten(),
+    keras.layers.Dense(512, activation='relu'),
+    keras.layers.BatchNormalization(),
+    keras.layers.Dropout(rate=0.25),
+    
+    keras.layers.Dense(43, activation='softmax')
+])
+
+model = keras.models.load_model(filepath='CNN.h5')
+#model = keras.models.load_model(filepath='CNN.h5')
+
+def pred(image):
+    image_numpy = np.array(image.resize((50,50))) / 255
+    image_numpy = np.expand_dims(image_numpy, axis=0)
+
+    predicted = model.predict(image_numpy)
+    namesign = classes[np.argmax(predicted, axis=-1)[0]]
+    print(namesign)
+
+    filename = 'speech.wav'
+    tts = gTTS(namesign)
+    tts.save(filename)
+    #display(Audio(filename, autoplay=True))
+    return namesign
+
+# Path: main.py
+if __name__ == '__main__':
+    image = Image.open('test.jpg')
+    res = pred(image)
+    print(res)
